d4c8ee443a92915bd115e9f0f0226dd41a0ffb72
Fachruddin <poenyoex@gmail.com>
2025-03-20 13:51:33 +0700

    chore: update filament notifications to version 3.3 and adjust navigation group labels for user management

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -24,7 +24,7 @@ class UserResource extends Resource
     protected static ?string $model = User::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-users';
-    protected static ?string $navigationGroup = 'General';
+    protected static ?string $navigationGroup = 'User Management';
     protected static ?string $navigationLabel = 'Users';
     protected static ?int $navigationSort = 1;
 
--- app/Filament/Widgets/UserStatsWidget.php
+++ app/Filament/Widgets/UserStatsWidget.php
@@ -2,7 +2,6 @@
 
 namespace App\Filament\Widgets;
 
-use App\Models\User;
 use Filament\Widgets\StatsOverviewWidget as Widget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Illuminate\Support\Facades\Auth;
@@ -12,16 +11,17 @@ class UserStatsWidget extends Widget
     protected function getStats(): array
     {
         $user = Auth::user();
-        
+        $roleName = $user->roles->first()->name ?? 'No Role';
+
         return [
-            Stat::make('Tipe Akun', $user->position ? $user->position->name : 'Belum ditetapkan')
-                ->description('Jabatan Anda dalam sistem')->icon('heroicon-o-user')
-                ->color('primary'),
-            
+            Stat::make('Positions', $roleName)
+            ->description('Jabatan Anda dalam sistem')->icon('heroicon-o-user')
+            ->color('primary'),
+
             Stat::make('Akun Dibuat', $user->created_at->isoFormat('D MMMM Y'))->icon('heroicon-o-calendar')
                 ->description($user->created_at->diffForHumans())
                 ->color('success'),
-            
+
             Stat::make('Login Terakhir', now()->isoFormat('D MMMM Y'))->icon('heroicon-o-clock')
                 ->description('Aktif sekarang')
                 ->color('warning'),
