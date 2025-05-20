1305f1e3c05c8ef0855a09cf839265a40168bf42
Fachruddin <poenyoex@gmail.com>
2025-05-20 15:17:01 +0700

    feat: Enhance InternsExport with institution type filtering and status calculation

--- app/Filament/Exports/InternsExport.php
+++ app/Filament/Exports/InternsExport.php
@@ -6,12 +6,46 @@ use App\Models\Intern;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithTitle;
 
-class InternsExport implements FromCollection, WithHeadings, WithMapping
+class InternsExport implements FromCollection, WithHeadings, WithMapping, WithTitle
 {
+    protected $institutionType;
+    
+    /**
+     * Constructor dengan parameter filter tipe institusi
+     * 
+     * @param string $institutionType
+     */
+    public function __construct($institutionType = 'all')
+    {
+        $this->institutionType = $institutionType;
+    }
+    
+    /**
+     * @return string
+     */
+    public function title(): string
+    {
+        return match($this->institutionType) {
+            'Perguruan Tinggi' => 'Data Magang Perguruan Tinggi',
+            'SMA/SMK' => 'Data Magang SMA/SMK',
+            default => 'Data Magang'
+        };
+    }
+    
     public function collection()
     {
-        return Intern::with('school')->get();
+        $query = Intern::with('school');
+        
+        // Filter berdasarkan tipe institusi
+        if ($this->institutionType !== 'all') {
+            $query->whereHas('school', function ($q) {
+                $q->where('type', $this->institutionType);
+            });
+        }
+        
+        return $query->get();
     }
 
     public function headings(): array
@@ -20,6 +54,7 @@ class InternsExport implements FromCollection, WithHeadings, WithMapping
             'ID',
             'Nama',
             'Email',
+            'Tipe Institusi',
             'Sekolah/Instansi',
             'Divisi',
             'No. Telepon',
@@ -28,15 +63,33 @@ class InternsExport implements FromCollection, WithHeadings, WithMapping
             'Telepon Pembimbing',
             'Tanggal Mulai',
             'Tanggal Selesai',
+            'Status',
         ];
     }
 
     public function map($intern): array
     {
+        // Menghitung status magang
+        $now = now();
+        $start = $intern->start_date;
+        $end = $intern->end_date;
+        $hampirStart = $end->copy()->subMonth();
+
+        if ($now->lessThan($start)) {
+            $status = 'Datang';
+        } elseif ($now->greaterThanOrEqualTo($hampirStart) && $now->lessThanOrEqualTo($end)) {
+            $status = 'Hampir';
+        } elseif ($now->between($start, $hampirStart->subSecond())) {
+            $status = 'Active';
+        } else {
+            $status = 'Selesai';
+        }
+        
         return [
             $intern->id,
             $intern->name,
             $intern->email,
+            $intern->school ? $intern->school->type : '',
             $intern->school ? $intern->school->name : '',
             $intern->division,
             $intern->no_phone,
@@ -45,6 +98,7 @@ class InternsExport implements FromCollection, WithHeadings, WithMapping
             $intern->college_supervisor_phone,
             $intern->start_date ? $intern->start_date->format('d/m/Y') : '',
             $intern->end_date ? $intern->end_date->format('d/m/Y') : '',
+            $status,
         ];
     }
 }
