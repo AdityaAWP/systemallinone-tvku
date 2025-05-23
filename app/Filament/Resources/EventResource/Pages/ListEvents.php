20d427e7d6e3b63a31513b1713da67eaf7f4b609
Fachruddin <poenyoex@gmail.com>
2025-05-23 14:44:24 +0700

    refactor: Comment out unused header actions and add CalendarWidget to header

--- app/Filament/Resources/EventResource/Pages/ListEvents.php
+++ app/Filament/Resources/EventResource/Pages/ListEvents.php
@@ -12,57 +12,65 @@ use App\Models\Event;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\Grid;
+use App\Filament\Widgets\CalendarWidget;
 
 class ListEvents extends ListRecords
 {
     protected static string $resource = EventResource::class;
 
-    protected function getHeaderActions(): array
+    // protected function getHeaderActions(): array
+    // {
+    //     return [
+    //         Action::make('create')
+    //             ->label('Tambah Event')
+    //             ->icon('heroicon-o-plus')
+    //             ->modalHeading('Tambah Event Baru')
+    //             ->modalWidth('4xl')
+    //             ->modalSubmitActionLabel('Simpan')
+    //             ->form([
+    //                 TextInput::make('name')
+    //                     ->label('Name')
+    //                     ->required()
+    //                     ->maxLength(255)
+    //                     ->columnSpan('full'),
+
+    //                 Grid::make(2)
+    //                     ->schema([
+    //                         DateTimePicker::make('starts_at')
+    //                             ->label('Mulai Pada')
+    //                             ->required()
+    //                             ->withoutSeconds()
+    //                             ->displayFormat('d M Y H:i'),
+
+    //                         DateTimePicker::make('ends_at')
+    //                             ->label('Berakhir Pada')
+    //                             ->required()
+    //                             ->withoutSeconds()
+    //                             ->displayFormat('d M Y H:i'),
+    //                     ])
+    //                     ->columnSpan('full'),
+
+    //                 Textarea::make('description')
+    //                     ->label('Deskripsi')
+    //                     ->rows(5)
+    //                     ->maxLength(500)
+    //                     ->required()
+    //                     ->columnSpan('full'),
+    //             ])
+    //             ->action(function (array $data) {
+    //                 Event::create($data);
+    //                 \Filament\Notifications\Notification::make()
+    //                     ->title('Event berhasil ditambahkan')
+    //                     ->success()
+    //                     ->send();
+    //             }),
+    //     ];
+    // }
+
+    protected function getHeaderWidgets(): array
     {
         return [
-            Action::make('create')
-                ->label('Tambah Event')
-                ->icon('heroicon-o-plus')
-                ->modalHeading('Tambah Event Baru')
-                ->modalWidth('4xl')
-                ->modalSubmitActionLabel('Simpan')
-                ->form([
-                    TextInput::make('name')
-                        ->label('Name')
-                        ->required()
-                        ->maxLength(255)
-                        ->columnSpan('full'),
-
-                    Grid::make(2)
-                        ->schema([
-                            DateTimePicker::make('starts_at')
-                                ->label('Mulai Pada')
-                                ->required()
-                                ->withoutSeconds()
-                                ->displayFormat('d M Y H:i'),
-
-                            DateTimePicker::make('ends_at')
-                                ->label('Berakhir Pada')
-                                ->required()
-                                ->withoutSeconds()
-                                ->displayFormat('d M Y H:i'),
-                        ])
-                        ->columnSpan('full'),
-
-                    Textarea::make('description')
-                        ->label('Deskripsi')
-                        ->rows(5)
-                        ->maxLength(500)
-                        ->required()
-                        ->columnSpan('full'),
-                ])
-                ->action(function (array $data) {
-                    Event::create($data);
-                    \Filament\Notifications\Notification::make()
-                        ->title('Event berhasil ditambahkan')
-                        ->success()
-                        ->send();
-                }),
+            CalendarWidget::class,
         ];
     }
 }
