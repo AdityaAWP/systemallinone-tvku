6cc371019b3766c1aa0a9ed5d423aa00ae665875
AdityaAWP <aditwpratama1@gmail.com>
2025-05-27 17:07:39 +0700

    refactor: Remove EditProfileIntern page and migrate to new namespace, update view structure

---
+++ app/Filament/InternPages/EditProfileIntern.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Filament\InternPages;
+
+use Filament\Pages\Page;
+
+class EditProfileIntern extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.intern-pages.edit-profile-intern';
+}
+
