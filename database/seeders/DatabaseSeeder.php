72f1d56c92b9acffb5d8bb43699c3489ce372e5d
Fachruddin <poenyoex@gmail.com>
2025-06-05 11:21:09 +0700

    feat: Add SuperAdminSeeder to DatabaseSeeder for initial data setup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,13 +6,11 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    public function run()
     {
         $this->call([
             RoleSeeder::class,
+            SuperAdminSeeder::class,
         ]);
     }
 }
