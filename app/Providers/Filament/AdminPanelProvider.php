0a5cc6dfcdf938fe1d50ac597512f7030d9138f0
Fachruddin <poenyoex@gmail.com>
2025-06-14 20:52:50 +0700

    feat: Register ManagerLeaveReminderWidget and MonthlyOvertimeReminderWidget in AdminPanelProvider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -20,6 +20,9 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use App\Filament\Widgets\ManagerLeaveReminderWidget;
+use App\Filament\Widgets\MonthlyOvertimeReminderWidget;
+
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -64,7 +67,8 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 UserStatsWidget::class,
-                \App\Filament\Widgets\ManagerLeaveReminderWidget::class,
+                ManagerLeaveReminderWidget::class,
+                MonthlyOvertimeReminderWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
