d6e02c367810c1427c984890c85610d9c7943e14
Fachruddin <poenyoex@gmail.com>
2025-06-17 13:41:48 +0700

    fix: Update logo display logic based on login status for improved user experience

--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -1,1 +1,9 @@
-<img src="{{ asset('images/tvku-logo.png') }}" alt="" width="80" height="80" style="margin-bottom: 10px; margin-top: 10px;">
+@php
+    // Cek apakah sedang di halaman login dengan melihat URL atau class body
+    $isLogin = request()->routeIs('filament.admin.auth.login') || str_contains(request()->url(), 'login');
+@endphp
+@if($isLogin)
+    <img src="{{ asset('images/tvku-logo.png') }}" alt="Logo" style="height: 100px; width: auto;">
+@else
+    <img src="{{ asset('images/tvku-logo.png') }}" alt="Logo" style="height: 50px; width: auto;">
+@endif
