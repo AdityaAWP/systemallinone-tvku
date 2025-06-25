1cbcdd0c420ebf7792d4b389a6aee40ebc87cd37
Fachruddin <poenyoex@gmail.com>
2025-06-25 16:19:58 +0700

    feat: Add change password functionality to EditIntern page

--- app/Filament/Resources/InternResource/Pages/EditIntern.php
+++ app/Filament/Resources/InternResource/Pages/EditIntern.php
@@ -5,6 +5,9 @@ namespace App\Filament\Resources\InternResource\Pages;
 use App\Filament\Resources\InternResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Notification;
+use Illuminate\Support\Facades\Hash;
 
 class EditIntern extends EditRecord
 {
@@ -14,6 +17,36 @@ class EditIntern extends EditRecord
     {
         return [
             Actions\DeleteAction::make()->label('Batal'),
+            Actions\Action::make('changePassword')
+                ->label('Ganti Password')
+                ->icon('heroicon-o-key')
+                ->color('warning')
+                ->modalHeading('Ganti Password Anak Magang')
+                ->modalWidth('md')
+                ->modalSubmitActionLabel('Update Password')
+                ->form([
+                    TextInput::make('new_password')
+                        ->label('Password Baru')
+                        ->password()
+                        ->required()
+                        ->minLength(8)
+                        ->helperText('Password minimal 8 karakter dan mengandung huruf besar, kecil, dan angka.'),
+                    TextInput::make('new_password_confirmation')
+                        ->label('Konfirmasi Password Baru')
+                        ->password()
+                        ->required()
+                        ->same('new_password'),
+                ])
+                ->action(function (array $data) {
+                    $this->record->update([
+                        'password' => Hash::make($data['new_password'])
+                    ]);
+                    Notification::make()
+                        ->title('Password berhasil diubah')
+                        ->success()
+                        ->send();
+                })
+                ->modalSubmitAction(fn ($action) => $action->color('warning')),
         ];
     }
     protected function getRedirectUrl(): string
