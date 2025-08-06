97e77bf0cb12b1130f08f38793a435842fee2843
AdityaAWP <aditwpratama1@gmail.com>
2025-08-07 01:40:20 +0700

    feat: Enhance PDFAssignmentController to generate dynamic SPP, SPK, and Invoice numbers, and update assignmentPDF view for improved document presentation

--- app/Http/Controllers/PDFAssignmentController.php
+++ app/Http/Controllers/PDFAssignmentController.php
@@ -6,6 +6,7 @@ use App\Models\Assignment;
 use Illuminate\Http\Request;
 use PDF;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
+use Carbon\Carbon;
 
 class PDFAssignmentController extends Controller
 {
@@ -17,20 +18,40 @@ class PDFAssignmentController extends Controller
             return redirect()->back()->with('error', 'Assignment not found.');
         }
 
-        // Initialize qrCode variable as null.
         $qrCode = null;
 
-        // Check if the assignment is approved. If so, generate the QR code.
         if ($assignment->approval_status === Assignment::STATUS_APPROVED) {
             $urlToXml = route('assignment.xml', ['id' => $assignment->id]);
             $qrCode = base64_encode(QrCode::format('png')->size(120)->generate($urlToXml));
         }
 
-        // Pass all data, including the potentially null qrCode, to the view.
+        $createdDate = Carbon::parse($assignment->created_date);
+        $year = $createdDate->year;
+        $monthRoman = $this->_numberToRoman($createdDate->month);
+
+        $assignmentTypeString = $this->_formatState($assignment, 'type');
+        $spp_ket = '/SPP-D/'; // Default
+        if ($assignmentTypeString == 'Free') {
+            $spp_ket = '/FREE/SPP-D/';
+        } elseif ($assignmentTypeString == 'Barter') {
+            $spp_ket = '/BARTER/SPP-D/';
+        }
+        $generatedSppNumber = $assignment->spp_number . $spp_ket . $monthRoman . '/TVKU/' . $year;
+
+        $generatedSpkNumber = $assignment->spk_number . '/SPK/' . $monthRoman . '/TVKU/' . $year;
+
+        $generatedInvoiceNumber = null;
+        if ($assignmentTypeString == 'Berbayar') {
+            $generatedInvoiceNumber = 'Invoice Nomor I-' . $assignment->spp_number . '/KEU/TVKU/' . $monthRoman . '/' . $year;
+        }
+
         $data = [
-            'title'      => 'Assignment Report',
-            'assignment' => $assignment,
-            'qrCode'     => $qrCode
+            'title'                  => 'Assignment Report',
+            'assignment'             => $assignment,
+            'qrCode'                 => $qrCode,
+            'generatedSppNumber'     => $generatedSppNumber,
+            'generatedSpkNumber'     => $generatedSpkNumber,
+            'generatedInvoiceNumber' => $generatedInvoiceNumber,
         ];
 
         $pdf = PDF::loadView('assignmentPDF', $data);
@@ -40,9 +61,6 @@ class PDFAssignmentController extends Controller
             ->header('Content-Disposition', 'attachment; filename="assignment-'.$id.'.pdf"');
     }
 
-    /**
-     * This endpoint remains protected. It will only return XML for approved assignments.
-     */
     public function generateXml($id)
     {
         $assignment = Assignment::find($id);
@@ -51,39 +69,10 @@ class PDFAssignmentController extends Controller
             return response('<error>Resource not found or not approved</error>', 404)->header('Content-Type', 'text/xml');
         }
 
-        // (The rest of the XML generation logic remains the same)
         $xml = new \SimpleXMLElement('<spp/>');
-        
-        $formatState = function ($model, $field) {
-            switch ($field) {
-                case 'type':
-                    return match ($model->type) {
-                        Assignment::TYPE_FREE => 'Free',
-                        Assignment::TYPE_PAID => 'Berbayar',
-                        Assignment::TYPE_BARTER => 'Barter',
-                        default => $model->type,
-                    };
-                case 'priority':
-                    return match ($model->priority) {
-                        Assignment::PRIORITY_NORMAL => 'Biasa',
-                        Assignment::PRIORITY_IMPORTANT => 'Penting',
-                        Assignment::PRIORITY_VERY_IMPORTANT => 'Sangat Penting',
-                        default => 'Biasa',
-                    };
-                case 'approval_status':
-                     return match ($model->approval_status) {
-                        Assignment::STATUS_APPROVED => 'Disetujui',
-                        Assignment::STATUS_PENDING => 'Menunggu',
-                        Assignment::STATUS_DECLINED => 'Ditolak',
-                        default => $model->approval_status,
-                    };
-                default:
-                    return '';
-            }
-        };
 
-        $xml->addChild('tanggal_dibuat', \Carbon\Carbon::parse($assignment->created_date)->format('d-m-Y'));
-        $xml->addChild('jenis', $formatState($assignment, 'type'));
+        $xml->addChild('tanggal_dibuat', Carbon::parse($assignment->created_date)->format('d-m-Y'));
+        $xml->addChild('jenis', $this->_formatState($assignment, 'type'));
         $xml->addChild('klien', $assignment->client);
         $xml->addChild('nomor_spp', $assignment->spp_number);
         $xml->addChild('nomor_spk', $assignment->spk_number);
@@ -91,11 +80,56 @@ class PDFAssignmentController extends Controller
         $xml->addChild('keterangan', $assignment->description);
         $xml->addChild('nominal', 'Rp. ' . number_format($assignment->amount, 0, ',', '.'));
         $xml->addChild('beban_marketing', $assignment->marketing_expense ? 'Rp. ' . number_format($assignment->marketing_expense, 0, ',', '.') : '');
-        $xml->addChild('deadline', \Carbon\Carbon::parse($assignment->deadline)->format('d F Y'));
-        $xml->addChild('prioritas', $formatState($assignment, 'priority'));
-        $xml->addChild('disetujui', $formatState($assignment, 'approval_status'));
-        $xml->addChild('tanggal_disetujui', $assignment->approved_at ? \Carbon\Carbon::parse($assignment->approved_at)->format('d-m-Y') : '');
+        $xml->addChild('deadline', Carbon::parse($assignment->deadline)->format('d F Y'));
+        $xml->addChild('prioritas', $this->_formatState($assignment, 'priority'));
+        $xml->addChild('disetujui', $this->_formatState($assignment, 'approval_status'));
+        $xml->addChild('tanggal_disetujui', $assignment->approved_at ? Carbon::parse($assignment->approved_at)->format('d-m-Y') : '');
 
         return response($xml->asXML(), 200)->header('Content-Type', 'text/xml');
+    }
+    
+    private function _formatState($model, $field)
+    {
+        switch ($field) {
+            case 'type':
+                return match ($model->type) {
+                    Assignment::TYPE_FREE => 'Free',
+                    Assignment::TYPE_PAID => 'Berbayar',
+                    Assignment::TYPE_BARTER => 'Barter',
+                    default => $model->type,
+                };
+            case 'priority':
+                return match ($model->priority) {
+                    Assignment::PRIORITY_NORMAL => 'Biasa',
+                    Assignment::PRIORITY_IMPORTANT => 'Penting',
+                    Assignment::PRIORITY_VERY_IMPORTANT => 'Sangat Penting',
+                    default => 'Biasa',
+                };
+            case 'approval_status':
+                 return match ($model->approval_status) {
+                    Assignment::STATUS_APPROVED => 'Disetujui',
+                    Assignment::STATUS_PENDING => 'Menunggu',
+                    Assignment::STATUS_DECLINED => 'Ditolak',
+                    default => $model->approval_status,
+                };
+            default:
+                return '';
+        }
+    }
+
+    private function _numberToRoman($number)
+    {
+        $map = ['M' => 1000, 'CM' => 900, 'D' => 500, 'CD' => 400, 'C' => 100, 'XC' => 90, 'L' => 50, 'XL' => 40, 'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1];
+        $returnValue = '';
+        while ($number > 0) {
+            foreach ($map as $roman => $int) {
+                if ($number >= $int) {
+                    $number -= $int;
+                    $returnValue .= $roman;
+                    break;
+                }
+            }
+        }
+        return $returnValue;
     }
 }
